a403b18f0b0310a3ed792a793c1a0e746ac92a37
Harry <harib1821@gmail.com>
2026-01-30 17:38:13 +0530

    Add auth_middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth_middleware');
 const User = require('../models/User');
 
 // 👉 REGISTER
