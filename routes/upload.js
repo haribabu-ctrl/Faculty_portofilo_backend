0d8e85326dc8850fe67c8a12baf2ff5cc70a7f2e
Harry <harib1821@gmail.com>
2026-01-31 14:40:24 +0530

    Update routes and server files

--- routes/upload.js
+++ routes/upload.js
@@ -40,8 +40,8 @@ router.post("/:table", upload.single("file"), async (req, res) => {
         employeeId: parseNum(row["Employee ID"]),
         courseName: row["Course Name"].trim(),
         semBranch: row["Sem-Branch-Sec"].trim(),
-        appearedStudents: parseNum(row["No. of students appeared (A)"]),
-        passedStudents: parseNum(row["No. of students Passed (B)"])
+        appearedStudents: parseNum(row["appeared"]),
+        passedStudents: parseNum(row["passed"])
       }));
 
     for (const doc of docs) {
